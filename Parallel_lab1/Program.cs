c5eb8782839cea03a957fc90eafefce1774731e9
Maxim <m.shakurov99@mail.ru>
2021-06-06 21:42:42 +0300

    final version

--- Parallel_lab1/Program.cs
+++ Parallel_lab1/Program.cs
@@ -11,44 +11,48 @@ namespace Parallel_lab1
     {
         static void Main(string[] args)
         {
-            var numThread = 5;
-            
+            int max_count_parallel = 0;
+            var numThread = 4;
+            var locker = new Object();
 
-            //int[] arr = new int[100000000];
-            int[] arr = new int[] { 1, 2, 0, 0, 0, 0, 0, 2, -3, 1, 4, 5, 6, 0, 0, 0, 5, 4,0,0 };
-            int chunks_array = (int)Math.Ceiling((decimal)arr.Length / numThread);
+            Console.Write("Введите количество потоков: ");
+            numThread = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[500000];
+
+            //int chunks_array = (int)Math.Ceiling((decimal)arr.Length / numThread); 
+            int chunks_array = arr.Length / numThread;
             int sum = 0;
-            int max_count_parallel = 0, max_count = 0, count = 0;
+            int max_count = 0;
 
-            //Random rnd = new Random();
-            //for (int j = 0; j < 100000000; j++)
-            //{
-            //    arr[j] = rnd.Next();
-            //}
-
-          
+            Random rnd = new Random();
+            for (int j = 0; j < 500000; j++)
+            {
+                arr[j] = rnd.Next();
+            }
 
 
+            for (int h = 0; h < 4; h++) { 
 
+            DateTime dold_not_par = DateTime.Now;
             #region not_parallel
             for (int i = 0; i < arr.Length; i++)
             {
-                sum += arr[i];
-                if (sum == 0)
+                sum = arr[i];
+                for(int j = i+1; j < arr.Length; j++)
                 {
-                    count++;
-                    if (count > max_count)
+                    sum += arr[j];
+                    if (sum == 0)
                     {
-                        max_count = count;
+                        if (j - i + 1 > max_count)
+                        {
+                            max_count = j - i + 1;
+                        }
                     }
                 }
-                else
-                {
-                    count = 0;
-                    sum = 0;
-                }
+                sum = 0;
             }
-
+            Console.WriteLine(DateTime.Now - dold_not_par);
             Console.WriteLine("Не параллельный результат - "+ max_count);
 
             #endregion
@@ -57,55 +61,66 @@ namespace Parallel_lab1
 
 
             DateTime dold = DateTime.Now;
-            var locker = new Object();
-            int index = 0;
-            var result = Parallel.For(1, numThread, (i, state) =>
+            
+
+
+
+            var result = Parallel.For(0, numThread, i =>
             {
+                int local_max_count_parallel = 0;
+                int local_sum = 0;
+                int startIndex = getStartIndex(i);
+                
 
-                    Console.WriteLine("Поток " + i.ToString() + " запущен");
-                    for (int j = 0; j < chunks_array; j++)
+                for (int k = 0; k < chunks_array; k++)
+                {
+                    local_sum = arr[startIndex+k];
+                    for (int j = startIndex +k+ 1; j < arr.Length; j++)
                     {
-                        if(i * chunks_array < arr.Length) 
-                        {
-                            index = i * chunks_array-1;
-                        }
-                        else 
-                        {
-                        index = arr.Length - (arr.Length - i * chunks_array);
-                        }
-                    Console.WriteLine(index);
-                        sum += arr[index];
-                    lock(locker)
-                    index++;
 
-                        if (sum == 0)
+                        local_sum += arr[j];
+                        if (local_sum == 0)
                         {
-                            count++;
-                        
-                            if (count > max_count_parallel)
+
+                            if (j - (startIndex +k)+ 1 > local_max_count_parallel)
                             {
-
-                                max_count_parallel = count;
+                                local_max_count_parallel = j - (startIndex+k) + 1;
                             }
-                        }
-                        else
-                        {
-                            count = 0;
-                            sum = 0;
-                        }
 
-                    
+                        }
+                    }
                 }
+                lock (locker)
+                {
+                    if (local_max_count_parallel > max_count_parallel)
+                    {
+                        max_count_parallel = local_max_count_parallel;
+                    }
+                }
+
             });
             Console.WriteLine( DateTime.Now - dold );
 
             Console.WriteLine("Параллельный результат - " + max_count_parallel);
             Console.ReadLine();
 
+            }
 
 
 
-
+            int getStartIndex(int iTread)
+            {
+                int index = 0;
+                if (iTread < chunks_array)
+                {
+                    index = iTread * chunks_array;
+                }
+                else
+                {
+                    index = arr.Length - (arr.Length - iTread * chunks_array) - 1;
+                }
+                return index;
+            }
 
 
 
